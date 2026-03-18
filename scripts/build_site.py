cf40f8b8ed562953504e599e7ba2cd498eb34c2e
Patel230 <Lakshmanp230@gmail.com>
2026-03-18 10:42:38 +0530

    style: yellow #facc15 accent, Fira Code font

--- scripts/build_site.py
+++ scripts/build_site.py
@@ -87,7 +87,7 @@ def main():
   <meta name="description" content="A self-evolving coding agent written in Go. Currently on Day {day_count}.">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
